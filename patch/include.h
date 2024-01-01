eb993d389c561f51a08aa277fccfd6378488f164
revel <leeroyjenkz777@gmail.com>
2024-01-01 16:55:32 -0400

    first

---
+++ patch/include.h
@@ -0,0 +1,30 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_dwmblocks.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_statuscmd.h"
+#include "bar_winicon.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "aspectresize.h"
+#include "attachx.h"
+#include "cfacts.h"
+#include "dragmfact.h"
+#include "stacker.h"
+#include "sticky.h"
+#include "togglefullscreen.h"
+#include "vanitygaps.h"
+#include "xrdb.h"
+/* Layouts */
+#include "layout_centeredmaster.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
